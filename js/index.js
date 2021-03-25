2a37d7cf3e8b26be5eb461ce51ed4017ac5ab910
Halyna-B <galya.berdik@gmail.com>
2021-03-25 17:12:55 +0200

    added fetch function and add json file

--- js/index.js
+++ js/index.js
@@ -1,24 +1,3 @@
-const hotToursJSON = `
-[
-{
-"imageURL": "norway-tour.jpg",
-"title": "Nature Hike in Lofoten",
-"description": "Make the most of your exploration of Austvågøya Island, one of the Lofoten Archipelago's main isles, during this 3-days guided walking tour. Learn about the landscape, flora, and fauna from your guide as you walk.",
-"price": 13500.00
-},
-{
-"imageURL": "norway-yacht-week.jpg",
-"title": "Bergen: Yacht Cruise",
-"description": "Make the most of your time in port on this shore excursion in Bergen. Departing from the center of Bergen, head along the narrow Mostraumen fjord to see some of western Norway's most spectacular scenery.",
-"price": 17900.00
-},
-{
-"imageURL": "norway-lofoten-islands.jpg",
-"title": "Mountain Trip - Moskenes",
-"description": "We will visit the famous and breathtaking mountain top, called Ryten in Moskenes municipality in Lofoten. Unique opportunity to enjoy a peaceful, quiet yet relaxing adventure in the Northern part of the world.",
-"price": 15399.00 
-}
-]`;
 
 
 function renderTours(tours, sortOder = "ascending"){
@@ -38,7 +17,14 @@ function renderTours(tours, sortOder = "ascending"){
    </article>`
 }
 
-renderTours(JSON.parse(hotToursJSON));
+
+async function fetchTours(order){
+    const response = await fetch('hotTours.json');
+    const tours = await response.json();
+    renderTours(tours, order);
+}
+
+fetchTours("ascending");
 
 const sortToursAscendingButtons = document.querySelector('.sort-asc');
 const sortToursDescendingButton = document.querySelector('.sort-dsc');
@@ -49,11 +35,11 @@ sortToursDescendingButton.addEventListener('click', sortToursDescending);
 function sortToursAscending() {
     sortToursDescendingButton.classList.remove('active');
     sortToursAscendingButtons.classList.add('active');
-renderTours(JSON.parse(hotToursJSON), "ascending");
+    fetchTours("ascending");
 };
 
 function sortToursDescending() {
     sortToursDescendingButton.classList.add('active');
     sortToursAscendingButtons.classList.remove('active');
-renderTours(JSON.parse(hotToursJSON), "descending");
+    fetchTours("descending");
 };
