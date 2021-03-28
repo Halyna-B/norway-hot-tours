99ad445d0ecbcab3bc86a7bf9ff3b06f364e35a9
Halyna-B <galya.berdik@gmail.com>
2021-03-28 10:20:49 +0300

    fixed convert problem

--- js/tours-list.js
+++ js/tours-list.js
@@ -3,22 +3,18 @@ class ToursList {
       this.container = document.querySelector('.hot-tours');
       this.toursService = new ToursService();
       this.currencyService = new CurrencyService;
+      this.option = document.querySelector('.select-value');
       this.sortDirection = 'ascending';
       this.toursService
         .getTours()
         .then(() => this.renderTours())
         .then(() => this.addEventListeners());    
+      
     }
     async renderTours() {
         let toursListDomString = '';
         const tours = await this.toursService.getTours();
-        document.querySelector('.convert').addEventListener('click', showOption);
-        function showOption() {
-          const option = document.querySelector('.select-value');
-          return option.value;
-        }
-
-        const rate = await this.currencyService.getCurrencyRate(showOption());
+        const rate = await this.currencyService.getCurrencyRate(this.option.value);
         [...tours]
           .sort( (a, b) => this.sortDirection === 'ascending' 
                              ? a.price - b.price
@@ -30,24 +26,33 @@ class ToursList {
             <p>${tour.description}</p>
                <div class="button-container">
                      <button class="info-btn"><span>Info</span></button>
-                     <button class="info-btn"><span>${(tour.price*rate).toFixed(2)} NOK </span></button>
+                     <button class="info-btn"><span>${(tour.price*rate).toFixed(2)} </span></button>
                </div>
        </article>`
     });
     this.container.innerHTML = toursListDomString;
   }
      async addEventListeners() {
+        document
+          .querySelector('#show-option').addEventListener('click', async () => {
+          this.option.value ;
+          await this.renderTours();
+          this.addEventListeners();
+        });
 
-        document.querySelector('.sort-asc').addEventListener('click', async () => {
+        document
+        .querySelector('.sort-asc').addEventListener('click', async () => {
         this.sortDirection = 'ascending';
         await this.renderTours();
         this.addEventListeners();
     });
-        document.querySelector('.sort-dsc').addEventListener('click', async () => {
+        document
+        .querySelector('.sort-dsc').addEventListener('click', async () => {
         this.sortDirection = 'descending';
         await this.renderTours();
         this.addEventListeners();
     });
+    
   }
 }
 
